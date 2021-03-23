b79185b3593621638ed9cd1d389b192d96cf1e2f
Bil <pbil@its.jnj.com>
2021-03-23 19:13:47 +0000

    Header added to the data set from separate file; boxplots ploted for each class for all 4 atributes; means calculated for each class and atribute

--- Code/analysis.py
+++ Code/analysis.py
@@ -3,8 +3,7 @@
 
 import pandas as pd 
 import os
-import csv
-
+import matplotlib.pyplot as plt
 
 THIS_FOLDER = os.path.dirname(os.path.abspath(__file__))
 DATA_PATH = os.path.join(THIS_FOLDER, '../Data/')
@@ -13,6 +12,18 @@ atribute_file = os.path.join(DATA_PATH, 'atribute.names')
 data_file = os.path.join(DATA_PATH, 'iris.data')
 
 af = pd.read_csv(atribute_file, header=None, squeeze=True, dtype=str)
-df = pd.read_csv(data_file, header=None, dtype=str)
-print(af)
-print(df)
+df = pd.read_csv(data_file, header=None)
+
+# As a header row use a Series read from the atribute_file:
+df.columns = af
+
+# Calculate mean values for each class
+mean_values = df.groupby('class').mean()
+print(mean_values)
+
+# plot a boxplot for each class for all 4 atributes:
+df.groupby('class').boxplot(rot=45, fontsize=8, figsize=(8,10))
+plt.show()
+
+
+#print(df)
