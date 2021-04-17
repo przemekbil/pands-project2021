b2963cc7ddb6f1a546faf059f1295d8004f9fb38
Bil <pbil@its.jnj.com>
2021-04-17 16:58:27 +0100

    Boxplots added to better explain the data than the histograms alone

--- Code/analysis.py
+++ Code/analysis.py
@@ -1,7 +1,8 @@
 # Program to analyse data from Iris dataset
 # Author: Przemyslaw Bil
 
-import pandas as pd 
+import pandas as pd
+import math
 import os
 import matplotlib.pyplot as plt
 import seaborn as sbr
@@ -22,36 +23,56 @@ def getdata(datapath):
     # Return data frame with added header
     return df
 
-
+# This function accepts a subset of the data with 2 columns: class description and one variable
+# The output is a text file with descriptive stats and the histogram
+# File name for the histogram and the text file are taken from the second column name
 def outsummary(subset, outpath):
     # get a name of the atribute from the name of the second column
     atribute = subset.columns[1]
+
     # calculate the mean of this attribute for every class
     stats = subset.groupby('class').mean()
+
     # calculate the standard deviation of this attribute for every class
     stats['Std dev'] = subset.groupby('class').std()
+
     # change the name of the attribute column to Mean
     stats = stats.rename(columns={atribute: 'Mean'})
 
-    # Change current folder to /Out folder
+    # Change current folder to /Out folder (as per https://docs.python.org/3/library/os.html#os-file-dir)
     os.chdir(outpath)
 
-    # Output calculated descriptive stats to separate files
+    # Output calculated descriptive stats to file name after the attribute name
     with open(atribute+".txt", "wt") as outfile:
         outfile.write("Summary for {}\n".format(atribute))
         # Output as perhttps://stackoverflow.com/questions/31247198/python-pandas-write-content-of-dataframe-into-text-file
         # and https://pandas.pydata.org/docs/reference/api/pandas.DataFrame.to_string.html
         stats.to_string(outfile)
     
-    # output histogram for each class
-    bin = (subset[atribute].max()-subset[atribute].min())/20
+    # The following part will output histogram, different coulous for each class
 
-    hist = sbr.histplot(subset, x=atribute, hue="class", binwidth=bin)
-    hist.figure.savefig(atribute+".png")
+    # First, calculate the width of the bins
+    # calculate histogram bin width: https://www.qimacros.com/histogram-excel/how-to-determine-histogram-bin-interval/#:~:text=Calculate%20the%20number%20of%20bins%20by%20taking%20the%20square%20root,)%20by%20the%20%23%20of%20bins.
+    #nrofbins = math.ceil(math.sqrt(subset.count()/3))
+    #binw = (subset[atribute].max()-subset[atribute].min())/nrofbins
+
+    # Create histogram using seaborn library
+    sbr.histplot(subset, x=atribute, hue="class")
+
+    # output the histogram to png file named after the attribute name
+    plt.savefig(atribute+" histogram.png", dpi=150)
+
     # Added as per https://stackoverflow.com/questions/57533954/how-to-close-seaborn-plots
     # Without plt.close(), each next histogram was printed with the data from the previous ones
     plt.close()
 
+    # because histograms for different classes are overlapping, it's better to use boxplot to show the data distribution
+    #https://seaborn.pydata.org/generated/seaborn.boxplot.html
+    sbr.boxplot(data=subset, y=atribute, x="class")
+
+    # output the BOXPLOT to png file named after the attribute name
+    plt.savefig(atribute+" boxplot.png", dpi=150)
+    plt.close()
 
 # initialize folder locations
 THIS_FOLDER = os.path.dirname(os.path.abspath(__file__))
@@ -68,20 +89,17 @@ for column in iris.columns:
         # get a subset as per https://pandas.pydata.org/docs/getting_started/intro_tutorials/03_subset_data.html
         outsummary(iris[['class', column]], outfolder)
 
+
+# Exploratory data analysis as per https://www.youtube.com/watch?v=FLuqwQgSBDw part 1, 2 and 3
+sbr.set_style("whitegrid")
+scatt = sbr.pairplot(iris, hue="class", height=3).add_legend()
+plt.savefig('scatter plot.png', dpi=150)
+plt.close()
+
+
 # call the function that recreates calculations from the classic Fisher paper
 fisheranalysys(iris, outfolder)
 
-'''
-# Exploratory data analysis as per https://www.youtube.com/watch?v=FLuqwQgSBDw part 1, 2 and 3
-sbr.set_style("whitegrid")
-sbr.pairplot(iris, hue="class", height=3)\
-    .add_legend()
-plt.show()
-'''
-
-# plot a boxplot for each class for all 4 atributes:
-# df.groupby('class').boxplot(rot=45, fontsize=8, figsize=(8,10))
-# plt.show()
 
 
 #print(df)
