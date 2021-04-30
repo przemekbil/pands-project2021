4c250e4e6ca36e1fb79e3d7db603dd11c62b9ad5
Przemyslaw Bil <g00398317@gmit.ie>
2021-04-30 19:30:24 +0100

    Classify all 150 data points using Fisher's method and count misclassifications

--- Code/fishermodule.py
+++ Code/fishermodule.py
@@ -9,15 +9,24 @@ import pandas as pd
 import os
 from dofmodule import *
 
+
+#define coefficients 
+# values for the coefficients taken from p186
+L1=-3.308998
+L2=-2.759132
+L3=8.866048
+L4=9.392551
+
+
+
 def fisheranalysys(df, outpath, outfilename, counter):
 
     # https://stackoverflow.com/questions/33768122/python-pandas-dataframe-how-to-multiply-entire-column-with-a-scalar
     fm = df.copy()
-    # values for the coefficients taken from p186
-    fm['sepal length'] = df['sepal length'].multiply(-3.308998)
-    fm['sepal width'] = df['sepal width'].multiply(-2.759132)
-    fm['petal length'] = df['petal length'].multiply(8.866048)
-    fm['petal width'] = df['petal width'].multiply(9.392551)
+    fm['sepal length'] = df['sepal length'].multiply(L1)
+    fm['sepal width'] = df['sepal width'].multiply(L2)
+    fm['petal length'] = df['petal length'].multiply(L3)
+    fm['petal width'] = df['petal width'].multiply(L4)
 
     # https://stackoverflow.com/questions/25748683/pandas-sum-dataframe-rows-for-given-columns
     col_list = list(fm)
@@ -47,3 +56,35 @@ def fisheranalysys(df, outpath, outfilename, counter):
     # save the histogram for the compound Fisher variable
     plt.savefig(counter.getFig('fisherFig1.png'), dpi=150)
     plt.close()
+
+    # classify records using fisfer's coefficients
+    compund['classification'] = 'None'
+
+    # iterate through dataframe as per https://stackoverflow.com/questions/16476924/how-to-iterate-over-rows-in-a-dataframe-in-pandas
+    for index, row in compund.iterrows():
+
+        # calculate absolute distance to each Classes mean
+        d_set=abs(row['sum']-tableix.at['Iris-setosa','Mean'])
+        d_ver=abs(row['sum']-tableix.at['Iris-versicolor','Mean'])
+        d_vir=abs(row['sum']-tableix.at['Iris-virginica','Mean'])
+        
+        # Find the closest one
+        closest = min(d_set, d_ver, d_vir)
+
+        # classify the Species based on the clossest mean
+        if closest==d_set:
+            compund.at[index, 'classification']='Iris-setosa'
+        elif closest==d_ver:
+            compund.at[index,'classification']='Iris-versicolor'
+        else:
+            compund.at[index,'classification']='Iris-virginica'
+        
+        
+        if row['class'] == compund.at[index,'classification']:
+            compund.at[index,'error'] = 0
+        else:
+            compund.at[index,'error'] = 1
+    
+    # Append classification errors summary to 'Summary.txt' file
+    with open(outfilename, "at") as outfile:
+        printtable("Table {}: Classification errors using Fishers method".format(counter.getTab()), compund.groupby('class')['error'].sum(), outfile)
